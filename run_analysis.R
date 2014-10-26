1ba3fe7ee767a567ce28d45d2c76f8692e3ffd5e
ankau7 <akaushik3@gmail.com>
2014-10-26 17:19:30 -0600

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -59,4 +59,5 @@ for (s in 1:numSubjects) {
         row = row+1
     }
 }
-write.table(result, "data_set_with_the_averages.txt")
+write.table(result, "data_set_with_the_averages.txt", row.names=FALSE)
+
